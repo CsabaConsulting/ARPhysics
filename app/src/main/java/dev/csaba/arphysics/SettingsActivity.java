899c6a8c0fc01b6d2d4c534b9f82511417ccfeec
MrCsabaToth <csaba.toth.us@gmail.com>
2020-06-21 16:50:39 -0700

    Forming SettingsActivity, removing AppBar (since it overlapped with the notch anyway and we'll have separate button). Activity still crashing though

--- app/src/main/java/dev/csaba/arphysics/SettingsActivity.java
+++ app/src/main/java/dev/csaba/arphysics/SettingsActivity.java
@@ -2,19 +2,12 @@ package dev.csaba.arphysics;
 
 import android.os.Bundle;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // add back arrow to actionbar
-        final ActionBar actionbar = getSupportActionBar();
-        if (actionbar != null) {
-            actionbar.setDisplayHomeAsUpEnabled(true);
-        }
 
         setContentView(R.layout.activity_settings);
         // display fragment
